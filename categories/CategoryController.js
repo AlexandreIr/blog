0b2e865cb5cf1bbdf70d7a9203978f8117cb4c7d
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-08 23:34:56 -0300

    feat: navbar and new cateogry card form created

--- categories/CategoryController.js
+++ categories/CategoryController.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/categories', (req, res)=>{
-    res.send('<h1>Categorias</h1>')
-});
-
 router.get('/admin/categories/new', (req, res)=>{
-    res.send('<h1>Nova categoria</h1>')
+    res.render('./admin/categories/new');
 
 });
 
