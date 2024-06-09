18c3d3aa8ea82f333a47d16c7d4250f3ba213746
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-09 10:17:57 -0300

    feat: CategoryController defined

--- categories/CategoryController.js
+++ categories/CategoryController.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const category = require('./Category');
+const slugify = require('slugify');
 
 router.get('/admin/categories/new', (req, res)=>{
     res.render('./admin/categories/new');
+});
 
+router.post('/admin/categories/new', (req, res)=>{
+    const title = req.body.title;
+    if(title!=null){
+        category.create({
+            title:title,
+            slug:slugify(title)
+        }).then(()=>{
+            res.redirect('/');
+        })
+    }else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;
