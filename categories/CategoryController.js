6994702c186c7643cf55834520d7e39e41c7e036
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-11 23:13:45 -0300

    feat: edit feature created on category

--- categories/CategoryController.js
+++ categories/CategoryController.js
@@ -4,6 +4,7 @@ const category = require('./Category');
 const slugify = require('slugify');
 const { raw } = require('body-parser');
 const { where } = require('sequelize');
+const Category = require('./Category');
 
 router.get('/admin/categories/new', (req, res)=>{
     res.render('./admin/categories/new');
@@ -16,7 +17,7 @@ router.post('/admin/categories/new', (req, res)=>{
             title:title,
             slug:slugify(title)
         }).then(()=>{
-            res.redirect('/');
+            res.redirect('/categories');
         })
     }else {
         res.redirect('/admin/categories/new');
@@ -48,5 +49,32 @@ router.post('/categories/delete/',(req, res)=>{
         res.redirect('/categories');
     }
 });
+
+router.get('/categories/edit/:id', (req, res)=>{
+    const id = req.params.id;
+    if(isNaN(id)){
+        res.redirect('/categories');
+    }
+    category.findByPk(id).then(cat=>{
+        if(cat!=null){
+            res.render('admin/categories/edit', {categ:cat});
+        } 
+        res.redirect('/categories');
+    }).catch(error=>{
+        res.redirect('/categories');
+    })
+});
+
+router.post('/categories/update',(req, res)=>{
+    const id = req.body.id;
+    const title = req.body.title;
+    Category.update({title:title, slug:slugify(title)}, {
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect('/categories');
+    });
+})
 
 module.exports = router;
