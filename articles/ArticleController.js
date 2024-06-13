35ff205ed9ef7fe59fc276e70abf5b0677fc826a
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-12 22:56:45 -0300

    feat: article pages created

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
+const slugify = require('slugify');
+const Article = require('./Article');
 
-router.get('/articles', (req, res)=>[
-    res.send('<h1>Rota de artigos</h1>')
-]);
+router.get('/articles', (req, res)=>{
+    Article.findAll({raw:true}).then(art=>{
+        res.render('./admin/articles/index', {
+            articles:art
+        });
+    })
+})
 
 router.get('/admin/article/new', (req, res)=>{
-    res.render('/admin/articles/new');
+    res.render('./admin/articles/new');
 });
+router.post('/admin/article/new', (req, res)=>{
+    const title = req.body.title;
+    const body = req.body.body;
+
+    if(title!=null && body!=null){
+        Article.create({
+            title:title,
+            slug:slugify(title),
+            body:body
+        }).then(()=>{
+            res.redirect('/articles');
+        })
+    } else {
+        res.redirect('/admin/article/new');
+    }
+});
+
 
 
 module.exports = router;
