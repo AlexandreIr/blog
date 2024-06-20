9aa7dec969fc5f6f9fbf0c58010acbae329db666
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-20 10:11:41 -0300

    feat: express session basic settings

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const connection = require('./db/db');
 
 const CategoryController = require('./categories/CategoryController');
