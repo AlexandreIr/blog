0c38fa0c107a9728db1d35868560bbe726a6753b
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-22 02:35:12 -0300

    feat: logout system

--- user/UsersController.js
+++ user/UsersController.js
@@ -86,4 +86,9 @@ router.post('/admin/users/delete/:id', (req, res)=>{
     }
 });
 
+router.get('/logout', (req, res)=>{
+    req.session.user=undefined;
+    res.redirect('/');
+})
+
 module.exports = router;
